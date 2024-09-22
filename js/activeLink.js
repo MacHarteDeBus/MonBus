f8b6c573c7470856a8cab9aa11499502cb6a75ec
MacHarteDeBus <147745044+MacHarteDeBus@users.noreply.github.com>
2024-09-22 15:27:26 +0200

    maj script

--- js/activeLink.js
+++ js/activeLink.js
@@ -1,17 +1,24 @@
-// Obtenir le chemin complet de l'URL actuelle
-var currentPath = window.location.pathname;
+document.addEventListener('DOMContentLoaded', function() {
+    // Obtenir le chemin complet de l'URL actuelle
+    var currentURL = window.location.href;
 
-// Sélectionner tous les liens dans la sidebar
-var menuItems = document.querySelectorAll('.sidebar ul li a');
+    console.log("URL actuelle:", currentURL); // Log de l'URL actuelle pour débogage
 
-// Boucler sur les liens pour comparer l'URL et ajouter la classe 'active'
-menuItems.forEach(function(item) {
-    // Obtenir le chemin relatif du lien
-    var linkPath = item.getAttribute('href');
+    // Sélectionner tous les liens dans la sidebar
+    var menuItems = document.querySelectorAll('.sidebar ul li a');
 
-    // Comparer le chemin de l'URL actuelle avec le chemin relatif du lien
-    if (currentPath.includes(linkPath)) {
-        // Ajouter la classe 'active' si l'URL actuelle contient le chemin du lien
-        item.parentElement.classList.add('active');
-    }
+    // Boucler sur les liens pour comparer l'URL et ajouter la classe 'active'
+    menuItems.forEach(function(item) {
+        // Obtenir l'URL complète du lien
+        var linkURL = new URL(item.href).href;
+
+        console.log("URL du lien:", linkURL); // Log de l'URL du lien pour débogage
+
+        // Comparer l'URL complète du lien avec l'URL actuelle
+        if (currentURL === linkURL) {
+            // Ajouter la classe 'active' si les URL correspondent
+            item.parentElement.classList.add('active');
+            console.log("Classe 'active' ajoutée à:", item.href); // Confirmation de l'ajout
+        }
+    });
 });
